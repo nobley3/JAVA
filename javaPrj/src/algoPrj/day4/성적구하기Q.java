77b980293707304767cbb0e149793d004a2ef1fc
nobley3 <121958909+nobley3@users.noreply.github.com>
2023-07-24 17:49:48 +0900

    0724

---
+++ javaPrj/src/algoPrj/day4/성적구하기Q.java
@@ -0,0 +1,21 @@
+package algoPrj.day4;
+
+import java.util.Scanner;
+
+public class 성적구하기Q {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		int kor,eng,math=0;
+		int avg,kor_avg,eng_avg,math_avg=0;
+		int sum,kor_sum,eng_sum,math_sum=0;
+		int MAX=60;
+		int subject=3;
+		
+		for(int i=0;i<MAX;i++) {
+			
+		}
+	}
+
+}
